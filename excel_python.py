d12146008a57536d8d7668dfeeefecca99d1ddba
Saimun <saimun4u@gmail.com>
2021-05-02 15:49:00 -0500

    #Reading excel files with python #Reading any specified cell data

--- excel_python.py
+++ excel_python.py
@@ -5,4 +5,23 @@ os.chdir('C:\\Users\\Saimun\\Python ardit\\reading excel xls')
 
 workbook = openpyxl.load_workbook('example.xlsx')
 
-print(type(workbook))
+# print(type(workbook))
+
+print(workbook.get_sheet_names())
+
+sheet = workbook.get_sheet_by_name('Sheet1')
+
+cell1 = sheet['B1'].value
+
+print(cell1)
+
+cell2 = sheet['C1'].value
+print(cell2)
+
+# print(sheet.cell(row=1, column=2).value)
+
+for i in range(1,8):
+    print(sheet.cell(row=i, column=2).value)
+
+for i in range(1,8):
+    print(sheet.cell(row=i, column=3).value)
